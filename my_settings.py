dc082bd97ffbe226b28447a98889b7eb76692f22
mini-777 <pho0570@gmail.com>
2020-11-19 17:36:00 +0900

    commit

--- my_settings.py
+++ my_settings.py
@@ -6,5 +6,8 @@ DATABASES = {
         'PASSWORD': 'dkssudgktpdy',
         'HOST': 'database-1.cip9531xqh6o.ap-northeast-2.rds.amazonaws.com',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
